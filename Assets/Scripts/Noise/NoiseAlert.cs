975bc906cd066bbd15863dfecf264355a8d378a5
masood <masood_a_t@yahoo.com>
2023-06-28 03:33:19 +0400

    Noise Cleanup
    Noise Cleanup

--- Assets/Scripts/Noise/NoiseAlert.cs
+++ Assets/Scripts/Noise/NoiseAlert.cs
@@ -8,78 +8,36 @@ namespace Assets.Scripts.Noise
     public class NoiseAlert : NoiseGenerator
     {
 
+        [SerializeField] private GameObject _alertObject;
+        [SerializeField] private float _alertDuration;
+        [SerializeField] private float _growUpAlertSpeed;
 
-        public GameObject AlertObject;
-        public float AlertTime;
-        public float AlertDuration;
-        public bool Alert;
+         private float _alertTime;
+         private bool _alert;
 
 
-        //public LayerMask DetectableMask;
-        [SerializeField] private float _speed;
 
-        //// Start is called before the first frame update
-        //void Start()
-        //{
-
-        //}
-
-        // Update is called once per frame
         void Update()
         {
-            if (Alert)
+            if (_alert)
             {
-                if (AlertTime > AlertDuration)
+                if (_alertTime > _alertDuration)
                 {
-                    Alert = false;
-                    AlertObject.transform.localScale = Vector3.zero;
+                    _alert = false;
+                    _alertObject.transform.localScale = Vector3.zero;
                 }
-                AlertObject.transform.localScale += Vector3.one * Time.deltaTime * _speed;
-                if (AlertObject.transform.localScale.x > 4)
-                    AlertObject.transform.localScale = Vector3.zero;
-                AlertTime += Time.deltaTime;
-            }
-
-
-            foreach (Enemy enemy in Enemes)
-            {
-                Ray ray = new Ray(transform.position, enemy.transform.position - transform.position);
-                Debug.DrawRay(ray.origin, ray.direction * AlertRadius);
+                _alertObject.transform.localScale += Vector3.one * Time.deltaTime * _growUpAlertSpeed;
+                if (_alertObject.transform.localScale.x > 4)
+                    _alertObject.transform.localScale = Vector3.zero;
+                _alertTime += Time.deltaTime;
             }
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            //Debug.Log("noise alert OnTriggerEnter");
-            AlertTime = 0;
-            //foreach (Enemy enemy in Enemes)
-            //{
-            //    //Debug.Log("noise alert enemy" + enemy.gameObject.name);
-            //    Ray ray = new Ray(transform.position, enemy.transform.position - transform.position);
-
-            //    if (Physics.Raycast(ray, out RaycastHit hit, AlertRadius))
-            //    {
-            //        //Debug.Log(hit.collider.gameObject.name, hit.collider.gameObject);
-            //        if (hit.collider.gameObject.CompareTag("Enemy"))
-            //        {
-            //            //Debug.Log("noise raycast hit to the Enemy");
-            //            enemy.controller.TransitionTo(enemy.controller.NoiseAlertState);
-            //            enemy.SetAlertPosition(transform.position);
-            //        }
-            //        //else
-            //        //Debug.Log("noise raycast hit noting");
-            //    }
-
-            //}
-
+            _alertTime = 0;
             MakeNoise();
-            Alert = true;
-
-
-
-
-
-
+            _alert = true;
         }
     }
 }
--- Assets/Scripts/Noise/NoiseGenerator.cs
+++ Assets/Scripts/Noise/NoiseGenerator.cs
@@ -7,19 +7,8 @@ namespace Assets.Scripts.Noise
 {
     public class NoiseGenerator : MonoBehaviour
     {
-        public float AlertRadius;
-        public List<Enemy> Enemes;
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        [SerializeField] private float AlertRadius;
+        [HideInInspector] public List<Enemy> Enemes;
 
         public void MakeNoise()
         {
@@ -30,20 +19,15 @@ namespace Assets.Scripts.Noise
         {
             foreach (Enemy enemy in Enemes)
             {
-                //Debug.Log("noise alert enemy" + enemy.gameObject.name);
                 Ray ray = new Ray(transform.position, enemy.transform.position - transform.position);
 
                 if (Physics.Raycast(ray, out RaycastHit hit, AlertRadius))
                 {
-                    //Debug.Log(hit.collider.gameObject.name, hit.collider.gameObject);
                     if (hit.collider.gameObject.CompareTag("Enemy"))
                     {
-                        //Debug.Log("noise raycast hit to the Enemy");
                         enemy.StateController.TransitionTo(enemy.StateController.NoiseAlertState);
                         enemy.SetAlertPosition(transform.position);
                     }
-                    //else
-                    //Debug.Log("noise raycast hit noting");
                 }
 
             }
